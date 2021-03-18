e882415b37358d1e253766186a9406245ac73e41
OwenMenzies <78829256+OwenMenzies@users.noreply.github.com>
2021-03-19 10:41:11 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    if (onoff == true) {
-        onoff = false
+    if (onoff == false) {
+        onoff = true
         basic.showLeds(`
             . . . . .
             . . . . #
@@ -10,8 +10,8 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.pause(100)
         basic.clearScreen()
-    } else if (onoff == false) {
-        onoff = true
+    } else if (onoff == true) {
+        onoff = false
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -23,15 +23,45 @@ input.onButtonPressed(Button.A, function () {
         basic.clearScreen()
     }
 })
+input.onButtonPressed(Button.B, function () {
+    if (flippy_boi == false) {
+        flippy_boi = true
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            # . # # .
+            # # . . .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+        servos.P0.setAngle(120)
+    } else if (flippy_boi == true) {
+        flippy_boi = false
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+        servos.P0.setAngle(30)
+    }
+})
 let moistness = 0
+let flippy_boi = false
 let onoff = false
-servos.P0.setAngle(90)
+servos.P0.setAngle(20)
 basic.showIcon(IconNames.SmallHeart)
 basic.clearScreen()
+onoff = false
+flippy_boi = false
 basic.forever(function () {
     while (onoff == true) {
-        moistness = pins.analogReadPin(AnalogPin.P0)
-        if (Math.map(moistness, 0, 1023, 0, 180) >= 70) {
+        moistness = pins.analogReadPin(AnalogPin.P1)
+        if (Math.map(moistness, 0, 1023, 0, 180) <= 70) {
             servos.P0.setAngle(120)
             basic.pause(3000)
             servos.P0.setAngle(30)
